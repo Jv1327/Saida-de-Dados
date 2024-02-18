3e6cb2d04feae96716d9aebf5de846ece31cf5aa
Jv1327 <jvrodrigues132770@gmail.com>
2024-02-18 08:12:27 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Oi, Tudo bem? Tenho 19 Anos e Programo.\n");
+
+    printf("Valor Inteiro: %d.\n", 10);
+    printf("Valor Real: %f.\n", 3.14159265);
+    printf("Valor Real com Apenas duas Casas: %.2f.\n", 3.14159265);
+    printf("Dado de Texto: %c.\n",'a');
+    printf("Dado de Texto: %s.\n", "testando");
+}
